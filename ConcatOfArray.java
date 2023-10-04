25b52ec6e68142a85270f2752e51da7d2d0f5071
Samuel Manzanares <samuel.manz57@gmail.com>
2023-10-04 14:58:08 -0500

    concatination of Array

---
+++ ConcatOfArray.java
@@ -0,0 +1,18 @@
+public class ConcatOfArray {
+    class Solution {
+        public int[] getConcatenation(int[] nums) {
+            
+            int numsLength = nums.length;
+            int[] ans = new int[numsLength*2];
+            
+    
+            for (int i = 0; i < numsLength; i++) {
+                ans[i] = nums[i];
+                ans[i + numsLength] = nums[i];
+            }
+            
+    
+            return ans;
+        }
+    }
+}
