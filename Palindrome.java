800592157c39b5864393fdcbbf09f97463925c95
Samuel Manzanares <samuel.manz57@gmail.com>
2023-10-04 14:12:59 -0500

    isAnagram

--- Palindrome.java
+++ Palindrome.java
@@ -11,13 +11,15 @@ class Palindrome {
             if (x < 0) {
                 return false;
             }
-    
+            
+            
             while (temp != 0) {
-                int digit = temp % 10;
-                reversed = reversed * 10 + digit;
-                temp /= 10;
+                int digit = temp % 10;  // stores last digit in temp
+                reversed = reversed * 10 + digit;   // appends digit to reversed
+                temp /= 10; // removes last digit in temp
             }
-    
+            
+            // checks if palindrone 
             if (reversed == x) {
                 return true;
             } else {
